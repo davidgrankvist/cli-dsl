c1ba530ad4b343cbad0a00332eb50a3fcf55ac82
David Grankvist <grankvist.david@gmail.com>
2024-10-26 17:08:07 +0200

    execute embedded scripts in subprocess

---
+++ CliDsl.App/Program.cs
@@ -0,0 +1,29 @@
+﻿using CliDsl.Lib.Engine;
+
+namespace CliDsl.App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            throw new ArgumentException("Please provider a script file name");
+        }
+
+        var scriptRunner = new SubProcessScriptRunner();
+        var interpreter = new Interpreter(scriptRunner);
+
+        var fileName = args[0];
+        if (!File.Exists(fileName))
+        {
+            throw new InvalidOperationException("File not found");
+        }
+
+        var scriptArgs = args.Where((_, i) => i != 0).ToArray();
+        using (var reader = new StreamReader(fileName))
+        {
+            interpreter.Run(reader, scriptArgs);
+        }
+    }
+}
---
+++ CliDsl.Lib/Engine/SubProcessScriptRunner.cs
@@ -0,0 +1,82 @@
+﻿using System.Diagnostics;
+
+using CliDsl.Lib.Execution;
+using CliDsl.Lib.Parsing;
+
+namespace CliDsl.Lib.Engine
+{
+    public class SubProcessScriptRunner : IScriptRunner
+    {
+        public void Run(AstScriptCommand command, List<string> parameters)
+        {
+            using (var process = CreateProcess(command, parameters))
+            {
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+
+        private Process CreateProcess(AstScriptCommand command, List<string> parameters)
+        {
+            Process process;
+            switch(command.Environment)
+            {
+                case ScriptEnvironment.PowerShell:
+                    process = CreatePowershellProcess(command, parameters);
+                    break;
+                case ScriptEnvironment.Batch:
+                    process = CreateCmdProcess(command, parameters);
+                    break;
+                case ScriptEnvironment.Sh:
+                    process = CreateShProcess(command, parameters);
+                    break;
+                case ScriptEnvironment.Bash:
+                    process = CreateBashProcess(command, parameters);
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unsupported script environment {command.Environment}");
+            }
+
+            return process;
+        }
+
+        private static Process CreatePowershellProcess(AstScriptCommand command, List<string> parameters)
+        {
+            return CreateProcess("powershell", $"-Command \"{command.Script}\"");
+        }
+
+        private static Process CreateCmdProcess(AstScriptCommand command, List<string> parameters)
+        {
+            return CreateProcess("cmd", $"/C \"{command.Script}\"");
+        }
+
+        private static Process CreateShProcess(AstScriptCommand command, List<string> parameters)
+        {
+            return CreateProcess("sh", $"-c \"{command.Script}\"");
+        }
+
+        private static Process CreateBashProcess(AstScriptCommand command, List<string> parameters)
+        {
+            return CreateProcess("bash", $"-c \"{command.Script}\"");
+        }
+
+        private static Process CreateProcess(string fileName, string arguments)
+        {
+            var startInfo = new ProcessStartInfo()
+            {
+                FileName = fileName,
+                Arguments = arguments,
+                RedirectStandardInput = false,
+                RedirectStandardOutput = false,
+                RedirectStandardError = false,
+                UseShellExecute = false,
+            };
+            var process = new Process()
+            {
+                StartInfo = startInfo,
+            };
+            return process;
+        }
+
+    }
+}
